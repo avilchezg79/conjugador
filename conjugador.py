3cea523366296154c08ecca8d2b1ee86842b3260
avilchezg79 <avilchezg@pucp.edu.pe>
2024-06-06 17:13:01 -0500

    upd

--- conjugador.py
+++ conjugador.py
@@ -2,6 +2,49 @@
 import pandas as pd
 verbos = pd.read_excel('verbos.xlsx')
 
+###############################################
+###############################################
+
+## archivo
+
+tiyay = pd.ExcelFile('tiyay.xlsx')
+
+## Diccionario para los pronombres
+## Creo un ciclo for que lea solo la hoja en la que coloquÈ los pronombres y la convierta a diccionario
+
+P = {}
+
+for hoja in tiyay.sheet_names:
+  pf = pd.read_excel('tiyay.xlsx', sheet_name = "pronombres")
+  pf = pf.set_index(['Unnamed: 0'])
+  p = pf.to_dict()
+  P[hoja] = p
+
+## Diccionario para los sufijos
+
+D = {}
+
+for hoja in tiyay.sheet_names:
+  df = pd.read_excel('tiyay.xlsx', sheet_name = hoja)
+  df = df.set_index(['Unnamed: 0'])
+  d = df.to_dict()
+  D[hoja] = d
+
+## FunciÛn para conjugar la base
+## Se llegar· al sufijo "abriendo" las distintas entradas en el orden seg˙n el que aparecen en el diccionario (tiempo > nro > persona)
+
+def conjuga(base, tiempo, numero, persona):
+    return base + D[tiempo][numero][persona]
+
+## FunciÛn para agregar el pronombre
+## Sigue la misma lÛgica que la funciÛn anterior
+
+def pronombre(tiempo, numero, persona):
+    return P[tiempo][numero][persona]
+
+###############################################
+###############################################
+
 ## diccionario
 quechua = list(verbos['quechua'])
 espanol = list(verbos['espa√±ol'])
@@ -11,19 +54,22 @@ dict_que_esp = dict(zip(quechua, espanol))
 ## importar streamlit
 import streamlit as st
 
-##selecci√≥n de la base
-option = st.selectbox(
+##seleccion de la base
+base = st.selectbox(
     'Seleccione un verbo en quechua', quechua)
-st.write('Seleccionaste', option)
+st.write('Seleccionaste', base)
 
-## selecci√≥n del tiempo
+## seleccion del tiempo
 tiempo = st.selectbox(
     'Elige un tiempo gramatical en quechua', ('presente simple', 'presente progresivo', 'presente habitual', 'pasado experimentado', 'pasado no experimentado'))
 
-## selecci√≥n del nro
+## seleccion del nro
 numero = st.selectbox(
     'Seleccione un n√∫mero gramatical en quechua', ('singular', 'plural'))
 
-## selecci√≥n de la persona
+## seleccion de la persona
 persona = st.selectbox(
     'Seleccione una persona gramatical en quechua', ('primera', 'segunda', 'tercera', 'cuarta (primera exclusiva)'))
+
+st.write('El verbo conjugado es', pronombre(tiempo, numero, persona)
+
